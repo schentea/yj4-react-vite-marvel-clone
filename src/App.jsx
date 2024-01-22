c202f0e8eaea8fc0b53f6f1123c6de90cb73b6ef
schentea <schentea@gmail.com>
2024-01-22 09:50:47 +0900

    test

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div className="text-2xl text-red-500">Hello</div>
-    </>
-  );
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainPage from "./routes/MainPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
